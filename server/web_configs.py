d48799f49d7e4296b5c114e012d94bcc628ee647
HinGwenWoong <peterhuang0323@qq.com>
2024-07-11 11:50:45 +0800

    Fix 数字人路径

--- server/web_configs.py
+++ server/web_configs.py
@@ -87,7 +87,7 @@ class WebConfigs:
     DIGITAL_HUMAN_VIDEO_OUTPUT_PATH: str = r"./work_dirs/digital_human/vid_output"
     DIGITAL_HUMAN_MODEL_DIR: str = r"./weights/digital_human_weights/"
     DIGITAL_HUMAN_BBOX_SHIFT: int = 0
-    DIGITAL_HUMAN_VIDEO_PATH: str = r"/root/hingwen_camp/work_dirs/lelemiao_digital_human_video.mp4"
+    DIGITAL_HUMAN_VIDEO_PATH: str = r"./doc/digital_human/lelemiao_digital_human_video.mp4"
     DIGITAL_HUMAN_FPS: str = 25
 
     # ==================================================================
