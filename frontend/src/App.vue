edd0683301f97249a943aedb801f62526014879b
HinGwenWoong <peterhuang0323@qq.com>
2024-08-02 20:49:19 +0800

    加入 Logo ，侧边栏支持 collapse

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,7 @@
 <template>
-  <el-container class="layout-container-demo" style="height: 100vh">
-    <el-aside width="200px">
-      <el-scrollbar>
-        <el-menu :router="true" :default-active="route.fullPath">
-          <el-menu-item index="/home">
-            <el-icon> <House /> </el-icon>首页
-          </el-menu-item>
-          <el-sub-menu index="/product">
-            <template #title>
-              <el-icon> <present /> </el-icon>商品管理
-            </template>
-            <el-menu-item index="/product/list">商品列表</el-menu-item>
-            <el-menu-item index="/product/add">新增商品</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="/system">
-            <template #title>
-              <el-icon> <setting /> </el-icon>系统配置
-            </template>
-            <el-menu-item index="/system/model">模型配置</el-menu-item>
-            <el-menu-item index="/system/word">敏感词配置</el-menu-item>
-            <el-menu-item index="/system/blacklist-question">疑问黑名单</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="/digital-human">
-            <template #title>
-              <el-icon> <User /> </el-icon>数字人配置
-            </template>
-            <el-menu-item index="/digital-human/list">角色管理</el-menu-item>
-            <el-menu-item index="/digital-human/upload">角色上传</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="/streaming">
-            <template #title>
-              <el-icon> <Mic /> </el-icon>直播配置
-            </template>
-            <el-menu-item index="/streaming/overview">开始直播</el-menu-item>
-          </el-sub-menu>
-          <el-sub-menu index="/order">
-            <template #title>
-              <el-icon> <ShoppingCart /> </el-icon>订单管理
-            </template>
-            <el-menu-item index="/order/overview">订单总览</el-menu-item>
-          </el-sub-menu>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
+  <el-container class="layout-container-demo">
+    <!-- 侧边栏 -->
+    <AslideComponent />
 
     <el-container>
       <el-header style="text-align: right; font-size: 12px">
@@ -87,16 +46,13 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { Menu as Present, User, Mic, Setting, House, ShoppingCart } from '@element-plus/icons-vue'
 
-import { useRoute } from 'vue-router'
+import AslideComponent from '@/components/AslideComponent.vue'
 import BreadCrumb from '@/components/BreadCrumb.vue'
 
 const username = ref('小明')
 const userAvatar = '@/assets/logo.png"'
 const avatarsStyle = { marginRight: '10px' }
-
-const route = useRoute()
 </script>
 
 <style scoped>
@@ -129,30 +85,12 @@ const route = useRoute()
   background: var(--el-color-primary-light-8);
 } */
 
-.layout-container-demo .el-menu {
-  border-right: none;
-}
-
-.layout-container-demo .el-main {
-  padding: 0;
-}
-
 .layout-container-demo .toolbar {
   display: inline-flex;
   align-items: center;
   justify-content: center;
   height: 100%;
   right: 20px;
-}
-
-/* 设置选中菜单项的背景色 */
-.el-menu-item.is-active {
-  background-color: #9997ee !important;
-  color: #ffffff;
-  /* 圆角的半径 */
-  border-radius: 10px;
-  /* 内边距 */
-  padding: -10px;
 }
 
 /* 
