1ac8b7266c21eea3cb2c542f5cd59e19145833d7
HinGwenWoong <peterhuang0323@qq.com>
2024-08-23 13:32:26 +0800

    完善主页，数字有递增动画

--- frontend/src/views/home/HomeView.vue
+++ frontend/src/views/home/HomeView.vue
@@ -1,17 +1,51 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { ElMessage } from 'element-plus'
-
+import { useTransition } from '@vueuse/core'
 import { getDashboardInfoRequest, type DashboardItem } from '@/api/dashboard'
 import BarChartComponent from '@/components/BarChartComponent.vue'
 import LineChartComponent from '@/components/LineChartComponent.vue'
 
 const systemInfo = ref({} as DashboardItem)
 
+const registeredBrandNum = ref(0) //入驻品牌方
+const productNum = ref(0) //商品数
+const dailyActivity = ref(0) //日活
+const todayOrder = ref(0) //订单量
+const totalSales = ref(0) //销售额
+const conversionRate = ref(0) //转化率
+
+// 配置动画
+const registeredBrandNumTrans = useTransition(registeredBrandNum, {
+  duration: 1500
+})
+const productNumTrans = useTransition(productNum, {
+  duration: 1500
+})
+const dailyActivityTrans = useTransition(dailyActivity, {
+  duration: 1500
+})
+const todayOrderTrans = useTransition(todayOrder, {
+  duration: 1500
+})
+const totalSalesTrans = useTransition(totalSales, {
+  duration: 1500
+})
+const conversionRateTrans = useTransition(conversionRate, {
+  duration: 1500
+})
+
 onMounted(async () => {
   const { data } = await getDashboardInfoRequest()
   if (data.code === 0) {
     systemInfo.value = data.data
+
+    registeredBrandNum.value = systemInfo.value.registeredBrandNum
+    productNum.value = systemInfo.value.productNum
+    dailyActivity.value = systemInfo.value.dailyActivity
+    todayOrder.value = systemInfo.value.todayOrder
+    totalSales.value = systemInfo.value.totalSales
+    conversionRate.value = systemInfo.value.conversionRate
   } else {
     ElMessage.error('获取总览数据失败')
   }
@@ -23,20 +57,23 @@ onMounted(async () => {
     <el-row>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">入驻品牌方数量</div>
-          <div class="info-item number">{{ systemInfo.registeredBrandNum }}</div>
+          <!-- <div class="info-item title">入驻品牌方数量</div> -->
+          <!-- <div class="info-item number">{{ systemInfo.registeredBrandNum }}</div> -->
+          <el-statistic title="入驻品牌方数量" :value="registeredBrandNumTrans" />
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">商品数</div>
-          <div class="info-item number">{{ systemInfo.productNum }}</div>
+          <!-- <div class="info-item title">商品数</div> -->
+          <!-- <div class="info-item number">{{ systemInfo.productNum }}</div> -->
+          <el-statistic title="商品数" :value="productNumTrans" />
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">日活</div>
-          <div class="info-item number">{{ systemInfo.dailyActivity }}</div>
+          <!-- <div class="info-item title">日活</div>
+          <div class="info-item number">{{ systemInfo.dailyActivity }}</div> -->
+          <el-statistic title="日活" :value="dailyActivityTrans" />
         </el-card>
       </el-col>
     </el-row>
@@ -44,20 +81,23 @@ onMounted(async () => {
     <el-row>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">今日订单数</div>
-          <div class="info-item number">{{ systemInfo.todayOrder }}</div>
+          <!-- <div class="info-item title">今日订单数</div>
+          <div class="info-item number">{{ systemInfo.todayOrder }}</div> -->
+          <el-statistic title="今日订单数" :value="todayOrderTrans" />
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">销售额</div>
-          <div class="info-item number">{{ systemInfo.totalSales }}</div>
+          <!-- <div class="info-item title">销售额</div>
+          <div class="info-item number">{{ systemInfo.totalSales }}</div> -->
+          <el-statistic title="销售额" :value="totalSalesTrans" />
         </el-card>
       </el-col>
       <el-col :span="8">
         <el-card shadow="never">
-          <div class="info-item title">转化率</div>
-          <div class="info-item number">{{ systemInfo.conversionRate }} %</div>
+          <!-- <div class="info-item title">转化率</div>
+          <div class="info-item number">{{ systemInfo.conversionRate }} %</div> -->
+          <el-statistic title="转化率" :value="conversionRateTrans" />
         </el-card>
       </el-col>
     </el-row>
